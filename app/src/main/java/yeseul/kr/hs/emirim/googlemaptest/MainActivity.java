adb26faa3a2e857cb3f69ab9abd40afe1ae8b5ff
leeysi <yeseul726@e-mirim.hs.kr>
2017-06-27 18:48:22 +0900

    #4 Event Programming

--- app/src/main/java/yeseul/kr/hs/emirim/googlemaptest/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/googlemaptest/MainActivity.java
@@ -3,11 +3,26 @@ package yeseul.kr.hs.emirim.googlemaptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
     }
 }
