f7c7d2f60b0bb52b0e4495d706c7f845e87822d8
leeysi <yeseul726@e-mirim.hs.kr>
2017-06-27 20:36:25 +0900

    #6 final Event Programming

--- app/src/main/java/yeseul/kr/hs/emirim/googlemaptest/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/googlemaptest/MainActivity.java
@@ -10,11 +10,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap googleMap;
     SupportMapFragment mapFragment;
+    GroundOverlayOptions loc_mark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +28,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         this.googleMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(19.993654, -155.824673), 17));
         googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                loc_mark = new GroundOverlayOptions();
+                loc_mark.image(BitmapDescriptorFactory.fromResource(R.drawable.logicon)).position(latLng, 100f, 100f);
+                googleMap.addGroundOverlay(loc_mark);
+            }
+        });
     }
 
     public static final int ITEM_SARELLITE=1;
     public static final int ITEM_NORMAL=2;
     public static final int ITEM_JAEHWANI=3;
     public static final int ITEM_JAEHWANII=4;
+    public static final int ITEM_MARK_CLEAR=5;
 
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -43,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SubMenu hotMenu = menu.addSubMenu("핫플");
         hotMenu.add(0, ITEM_JAEHWANI, 0, "♥재환이와의 신혼여행지♥");
         hotMenu.add(0, ITEM_JAEHWANII, 0, "♥재환이와의 신혼집♥");
+        menu.add(0, ITEM_MARK_CLEAR, 0, "위치 아이콘 제거");
 
         /*menu.add(0, 3, 0, "♥재환이와의 신혼여행지♥");*/
 
@@ -61,6 +75,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             case ITEM_JAEHWANI: googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(19.993654, -155.824673), 17));
                 return true;
             case ITEM_JAEHWANII: googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.591057, 127.059119), 17));
+                return true;
+            case ITEM_MARK_CLEAR: googleMap.clear();
                 return true;
         }
 
